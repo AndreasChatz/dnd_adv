7f9f59902e55572ad14a7366229680e07cace5d4
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-01-28 20:23:55 +0200

    Add Weapon class

--- items.py
+++ items.py
@@ -5,7 +5,7 @@ class Item():
 		self.value = value
 
 	def __str__(self):
-		return f"{self.name} : {self.description}\n{self.value}"
+		return f"{self.name}\nDescription: {self.description}'\nValue: {self.value}"
 
 
 class Gold(Item):
@@ -14,3 +14,13 @@ class Gold(Item):
 		super().__init__(name = 'Gold',
 						description = f'{self.amount} round shiny coins of Gold',
 						value = self.amount)
+
+
+class Weapon(Item):
+	def __init__(self, name, description, value, damage):
+		self.damage = damage
+		super.__init__(name, description, value)
+
+	def __str__(self):
+		return f'{name}\nDescription: {description}\nDamage: {self.damage}',
+				'\nValue: {self.value}'
